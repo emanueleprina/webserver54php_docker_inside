f62eb0df62b7bf3ace66c9c558f1962ff0d81332
emanueleprina <68809384+emanueleprina@users.noreply.github.com>
2021-05-25 11:57:39 +0200

    Update setup.sh

--- manual setup/setup.sh
+++ manual setup/setup.sh
@@ -4,4 +4,5 @@ a2enmod speling
 apt-get update 
 apt-get -y install cron nano wget
 mv apache2.conf /etc/apache2/apache2.conf 
- 
+mv vtigercron.sh /var/www/html/vtiger/vtigerst/cron/vtigercron.sh
+mv vtigercron.sh /var/www/html/vtiger/vtigerqa/cron/vtigercron.sh
